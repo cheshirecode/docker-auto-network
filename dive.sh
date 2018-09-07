ee662c0ca60c368e92d9e7c207e2e36d46360235
cheshireCode <dac4158@gmail.com>
2018-09-07 12:35:52 +0800

    move things aroudn

--- dive.sh
+++ dive.sh
@@ -8,18 +8,34 @@ n="${n:- 0}"
 # The dind image
 dind="${dind:-cheshirecode/dind-with-compose}"
 
+isDind="${isDind:-}"
+
 ##
 # Run a docker command by arbitrarily nesting dind
 # until I get bored and finally run your command.
 c=0
-dive() {
+
+# Docker-outside-Docker
+diveDood() {
     if (( n-- > 0 )); then
         c=$((c+1))
-        echo "Level $c" 
-        dive run --rm -t -v /var/run/docker.sock:/var/run/docker.sock --privileged=true "$dind" docker "$@"
+        echo "DooD level $c" 
+        diveDood run --rm -t -v /var/run/docker.sock:/var/run/docker.sock "$dind" docker "$@"
     else
         exec docker "$@"
     fi
 }
 
-dive "$@"
+# Docker-inside-Docker
+diveDind() {
+    echo "not yet working, need to collect container IDs in an array, execute and clean up"
+    exit 0
+    if (( n-- > 0 )); then
+        c=$((c+1))
+        echo "DinD level $c"
+        # diveDind run --rm -t --privileged=true docker:dind
+    else
+        exec docker "$@"
+    fi
+}
+[[ ! -z $isDind ]] && diveDind "$@" || diveDood "$@"
